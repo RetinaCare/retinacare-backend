ae7d5e55dd6fe00f592ef7551089e7f9f4a3e142
dev-xero <ssh.xero@gmail.com>
2025-11-18 21:52:04 +0100

    infra: add compose build scripts

---
+++ scripts/build-compose.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Building: mvn
+if ./mvnw clean -DskipTests package; then
+  echo "Jar built successfully"
+else
+  echo "Jar build failed"
+  exit 1
+fi
+
+# We use compose to spin up services
+if [ $2 = "--compose" ]; then
+  docker-compose down && docker-compose up --build
+fi
