e3ead0e633c27148f670114b21441ee1ff78a2e9
dev-xero <ssh.xero@gmail.com>
2025-11-20 16:19:35 +0100

    feat(db): model refresh token entity
    Signed-off-by: dev-xero <ssh.xero@gmail.com>

---
+++ src/main/java/org/retina/care/backend/domain/models/RefreshTokenEntity.java
@@ -0,0 +1,33 @@
+package org.retina.care.backend.domain.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long refreshTokenId;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    public boolean isExpired() {
+        return Instant.now().isAfter(this.expiryDate);
+    }
+}
