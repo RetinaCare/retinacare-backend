e8cd262f32add7160c72fa34857d0bf6aad7e87b
dev-xero <ssh.xero@gmail.com>
2025-11-21 19:21:08 +0100

    feat: implement refresh functionality
    Signed-off-by: dev-xero <ssh.xero@gmail.com>

--- src/main/java/org/retina/care/backend/application/services/AuthService.java
+++ src/main/java/org/retina/care/backend/application/services/AuthService.java
@@ -8,24 +8,35 @@ import org.retina.care.backend.core.infra.security.AccessToken;
 import org.retina.care.backend.core.infra.security.JwtService;
 import org.retina.care.backend.domain.models.RefreshTokenEntity;
 import org.retina.care.backend.domain.models.UserEntity;
+import org.retina.care.backend.domain.repositories.RefreshTokenRepository;
 import org.retina.care.backend.domain.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
     private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final RefreshTokenService refreshTokenService;
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     public AuthService(
             UserRepository userRepository,
+            RefreshTokenRepository refreshTokenRepository,
             PasswordEncoder passwordEncoder,
             JwtService jwtService,
             RefreshTokenService refreshTokenService
     ) {
         this.userRepository = userRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
         this.refreshTokenService = refreshTokenService;
@@ -52,7 +63,7 @@ public class AuthService {
 
         // User has been persisted, next we generate access and
         // refresh tokens.
-        AccessToken accessToken = this.jwtService.generateAccessToken(newUser.getFullname());
+        AccessToken accessToken = this.jwtService.generateAccessToken(newUser.getEmail());
         RefreshTokenEntity refreshToken = this.refreshTokenService.createRefreshToken(newUser);
 
         // Final response, we should NOT get any issues.
@@ -82,7 +93,7 @@ public class AuthService {
         }
 
         // As usual, access and refresh tokens are generated.
-        AccessToken accessToken = this.jwtService.generateAccessToken(savedUser.getFullname());
+        AccessToken accessToken = this.jwtService.generateAccessToken(savedUser.getEmail());
         RefreshTokenEntity refreshToken = this.refreshTokenService.createRefreshToken(savedUser);
 
         // Final response.
@@ -96,6 +107,48 @@ public class AuthService {
                         refreshToken.getToken(),
                         accessToken.getExpiryDate(),
                         refreshToken.getExpiryDate()
+                )
+        );
+    }
+
+    @Transactional
+    public RefreshResponseDto refreshAccess(@Valid RefreshRequestDto dto) throws Exception {
+        // Firstly, we MUST check to see that this userId and refresh token pair
+        // exists in our database.
+        RefreshTokenEntity refreshToken = refreshTokenRepository
+                .findByToken(dto.getRefreshToken())
+                .orElseThrow(() -> new BadRequestException("Token expired or invalid"));
+
+        if (refreshToken.isExpired()) {
+            refreshTokenRepository.delete(refreshToken);
+            // Again, the ambiguity here is so that attackers don't know exactly
+            // which is the case, whether invalid or expired.
+            throw new BadRequestException("Token expired or invalid");
+        }
+
+        // A lot of chaining here, but essentially, if the userId string from
+        // the refresh token in our database doesn't match the request userId,
+        // we deny them refresh.
+        if (!refreshToken.getUser().getUserId().toString().equals(dto.getUserId())) {
+            logger.warn("UserId {} wrongly requested refresh with TokenId {}", dto.getUserId(), refreshToken.getRefreshTokenId());
+            throw new BadRequestException("Token expired or invalid");
+        }
+
+        // Token rotation + refresh.
+        AccessToken accessToken = this.jwtService.generateAccessToken(refreshToken.getUser().getEmail());
+        RefreshTokenEntity newRefreshToken = this.refreshTokenService.createRefreshToken(refreshToken.getUser());
+
+        // Invalidating expired tokens.
+        refreshTokenRepository.delete(refreshToken);
+        refreshTokenRepository.save(newRefreshToken);
+
+        return new RefreshResponseDto(
+                new AuthPayload(
+                        "Bearer",
+                        accessToken.getToken(),
+                        newRefreshToken.getToken(),
+                        accessToken.getExpiryDate(),
+                        newRefreshToken.getExpiryDate()
                 )
         );
     }
